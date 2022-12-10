8be8035132db248f0ceda39afb00d2e2522ce40f
si.saaref <achmad.syarif@bvarta.com>
2022-12-10 17:39:58 +0700

    feat: create game mode option

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -6,6 +6,7 @@ import '../style/style.css';
 export default function Board() {
 	const [firstPlayerLine, setFirstPlayerLine] = useState([]);
 	const [secondPlayerLine, setSecondPlayerLine] = useState([]);
+	const [gameMode, setGameMode] = useState('computer');
 
 	const isFirstRender = useRef(true);
 
@@ -38,19 +39,18 @@ export default function Board() {
 
 	const secondPlayerTurn = () => {
 		let idSquare = getRandInt(0, 8);
-		console.log(
-			'CHECK TRUE FALSE =>',
-			firstPlayerLine.includes(idSquare),
-			'==',
-			secondPlayerLine,
-			idSquare
-		);
+		// console.log('CHECK TRUE FALSE =>', firstPlayerLine.includes(idSquare), '==', idSquare);
 		while (firstPlayerLine.includes(idSquare) || secondPlayerLine.includes(idSquare)) {
 			idSquare = getRandInt(0, 8);
 		}
 		// idSquare =  ? getRandInt(0, 8) : idSquare;
 		setSecondPlayerLine([...secondPlayerLine, idSquare]);
 		console.log('SECOND PLAYER TURN ID => ', idSquare);
+	};
+
+	const changeGameMode = (value) => {
+		setGameMode(value);
+		console.log('GAME MODE =>', gameMode, value);
 	};
 
 	const resetGamePlay = () => {
@@ -62,10 +62,36 @@ export default function Board() {
 		<>
 			<div className='board'>
 				<div className='board-info-section'>
-					<h1>AMADIKETU</h1>
-					<button className='btn-reset' onClick={resetGamePlay}>
-						Reset
-					</button>
+					<h1>Ticatoee</h1>
+					<div className=''>
+						<button className='btn-reset' onClick={resetGamePlay}>
+							Reset
+						</button>
+						<select
+							name=''
+							id=''
+							className='mode-game-selection'
+							onChange={(e) => changeGameMode(e.target.value)}
+						>
+							{['Computer', 'PvP'].map((item, id) => {
+								return (
+									<option id={id} key={id} value={`${item.toLowerCase()}`}>
+										{item}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+				</div>
+				<div className='board-winner-info-section'>
+					<div className='first-player-info'>
+						<h1>{gameMode === 'computer' ? 'You' : 'Player 1'}</h1>
+						{/* <h2>Score</h2> */}
+					</div>
+					<div className='second-player-info'>
+						<h1>{gameMode === 'computer' ? 'Computer' : 'Player 2'}</h1>
+						{/* <h2>Score</h2> */}
+					</div>
 				</div>
 				<div className='board-square-section'>
 					{secondPlayerLine.length === 3 && <div className='blocker'></div>}
