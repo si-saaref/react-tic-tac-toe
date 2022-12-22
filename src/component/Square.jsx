78b610d7c601716ce908d61057c75b8159e09749
si-saaref <achmadsyarif241@gmail.com>
2022-12-22 11:09:10 +0700

    rebaseing dev

--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -7,13 +7,8 @@ export default function Square({ onClick, oActive, xActive }) {
 
 	return (
 		<div className='square' onClick={onClick}>
-<<<<<<< HEAD
 			{oActive && <span className='oActive'>O</span>}
 			{xActive && <span className='xActive'>X</span>}
-=======
-			{oActive && <p>O</p>}
-			{xActive && <p>X</p>}
->>>>>>> d187922 (feat: create logic to check winner and show player choices)
 		</div>
 	);
 }
